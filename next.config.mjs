f61fa2a68ddb050f11b8c2c6e2b845116df85a2d
mirul22 <amirulikmal.biz@gmail.com>
2024-02-17 20:58:15 +0800

    update remove require and use import PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,22 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    dest: "public",
-    fallbacks: {
-      //image: "/static/images/fallback.png",
-      document: "/offline", // if you want to fallback to a custom page rather than /_offline
-      // font: '/static/font/fallback.woff2',
-      // audio: ...,
-      // video: ...,
-    },
-    workboxOptions: {
-      disableDevLogs: true,
-    },
-    // ... other options you like
-  });
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    // ... other options you like
-  };
-  
-  module.exports = withPWA(nextConfig);
+
+import withPWA from "@ducanh2912/next-pwa";
+
+const nextConfig = {
+  // ... other options you like
+};
+
+// Export using the ES module syntax
+export default withPWA({
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    document: "/offline", // if you want to fallback to a custom page rather than /_offline
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+}, nextConfig);
